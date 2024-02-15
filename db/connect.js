8608d82b7fdecd9ad026594de82f881b5fde9af5
Md. Ishraf Islam <ishrafislam2727@gmail.com>
2024-02-15 21:01:15 +0600

    Added provision for Vercel #2

--- db/connect.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const config = require('../config/config')
-
-mongoose.connect(config.MONGODB_URI)
-.then(() => console.log('Connected to MongoDB'))
-.catch(error => console.log('Error connecting to MongoDB: ', error))
-
-module.exports = mongoose
--- routes/user.js
+++
@@ -1,8 +0,0 @@
-const authMiddleware = require('../middleware/auth')
-const userController = require('../controllers/user')
-const express = require('express')
-const router = express.Router()
-
-router.get('/v1/info', authMiddleware.authenticate, userController.userInfo)
-
-module.exports = router
