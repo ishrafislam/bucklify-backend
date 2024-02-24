75c6f515566da582e1dd1035d92aef467cb1955f
Md. Ishraf Islam <ishrafislam2727@gmail.com>
2024-02-24 21:03:09 +0600

    Added password reset feature
    Signed-off-by: Md. Ishraf Islam <ishrafislam2727@gmail.com>

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+
 const userController = {
     async userInfo(req, res) {
         res.status(200).json(
@@ -10,6 +12,27 @@ const userController = {
                 }
             }
         )
+    },
+    async resetPassword(req, res) {
+        try {
+            const {old_password, new_password} = req.body
+
+            if (!await bcrypt.compare(old_password, req.user.password)) {
+                return res.status(400).json({success: false, data: {message: 'Invalid credentials'}})
+            }
+
+            if (old_password == new_password) {
+                return res.status(400).json({success: false, data: {message: 'New password can\'t be same as old password'}})
+            }
+
+            req.user.password = await bcrypt.hash(new_password, 5)
+            req.user.save()
+
+            res.status(200).json({success: true, data: {message: 'Updated the password successfully'}})
+        } catch(error) {
+            console.error('Error updating password of user:', error)
+            res.status(500).json({success: false, data: {message: 'Internal server error'}})
+        }
     }
 }
 
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/v1/info', authMiddleware.authenticate, userController.userInfo)
+router.patch('/v1/reset-password', authMiddleware.authenticate, userController.resetPassword)
 
 module.exports = router
